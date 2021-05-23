56c3c860abe49ad5a82570b44ccad039bccbea53
Nikolay Stoychev <nikolay@orionswave.com>
2021-05-23 08:53:02 +0200

    Updated formatting

--- assignments/A9/soundDownload.py
+++ assignments/A9/soundDownload.py
@@ -2,16 +2,21 @@ import os, sys
 import freesound as fs
 import json
 
-descriptors = [ 'lowlevel.spectral_centroid.mean',
-                'lowlevel.spectral_contrast.mean',
-                'lowlevel.dissonance.mean',
-                'lowlevel.hfc.mean',
-                'lowlevel.mfcc.mean',
-                'sfx.logattacktime.mean',
-                'sfx.inharmonicity.mean']
+descriptors = [
+    'lowlevel.spectral_centroid.mean', 'lowlevel.spectral_contrast.mean',
+    'lowlevel.dissonance.mean', 'lowlevel.hfc.mean', 'lowlevel.mfcc.mean',
+    'sfx.logattacktime.mean', 'sfx.inharmonicity.mean'
+]
 
-def downloadSoundsFreesound(queryText = "", tag=None, duration=None, API_Key = "", outputDir = "", topNResults = 5, featureExt = '.json'):
-  """
+
+def downloadSoundsFreesound(queryText="",
+                            tag=None,
+                            duration=None,
+                            API_Key="",
+                            outputDir="",
+                            topNResults=5,
+                            featureExt='.json'):
+    """
   This function downloads sounds and their descriptors from freesound using the queryText and the 
   tag specified in the input. Additionally, you can also specify the duration range to filter sounds 
   based on duration.
@@ -35,104 +40,130 @@ def downloadSoundsFreesound(queryText = "", tag=None, duration=None, API_Key = "
         containing sound-ids and freesound links for all the downloaded sounds in the outputDir. 
         NOTE: If the directory outputDir/queryText exists, it deletes the existing contents 
         and stores only the sounds from the current query. 
-  """ 
-  
-  # Checking for the compulsory input parameters
-  if queryText == "":
-    print("\n")
-    print("Provide a query text to search for sounds")
-    return -1
-    
-  if API_Key == "":
-    print("\n")
-    print("You need a valid freesound API key to be able to download sounds.")
-    print("Please apply for one here: www.freesound.org/apiv2/apply/")
-    print("\n")
-    return -1
-    
-  if outputDir == "" or not os.path.exists(outputDir):
-    print("\n")
-    print("Please provide a valid output directory. This will be the root directory for storing sounds and descriptors")
-    return -1    
-  
-  # Setting up the Freesound client and the authentication key
-  fsClnt = fs.FreesoundClient()
-  fsClnt.set_token(API_Key,"token")  
-  
-  # Creating a duration filter string that the Freesound API understands
-  if duration and type(duration) == tuple:
-    flt_dur = " duration:[" + str(duration[0])+ " TO " +str(duration[1]) + "]"
-  else:
-    flt_dur = ""
- 
-  if tag and type(tag) == str:
-    flt_tag = "tag:"+tag
-  else:
-    flt_tag = ""
+  """
 
-  # Querying Freesound
-  page_size = 30
-  if not flt_tag + flt_dur == "":
-    qRes = fsClnt.text_search(query=queryText ,filter = flt_tag + flt_dur,sort="score", fields="id,name,previews,username,url,analysis", descriptors=','.join(descriptors), page_size=page_size, normalized=1)
-  else:
-    qRes = fsClnt.text_search(query=queryText ,sort="score",fields="id,name,previews,username,url,analysis", descriptors=','.join(descriptors), page_size=page_size, normalized=1)
-  
-  outDir2 = os.path.join(outputDir, queryText)
-  if os.path.exists(outDir2):             # If the directory exists, it deletes it and starts fresh
-      os.system("rm -r " + outDir2)
-  os.mkdir(outDir2)
+    # Checking for the compulsory input parameters
+    if queryText == "":
+        print("\n")
+        print("Provide a query text to search for sounds")
+        return -1
 
-  pageNo = 1
-  sndCnt = 0
-  indCnt = 0
-  totalSnds = min(qRes.count,200)   # System quits after trying to download after 200 times
-  
-  # Creating directories to store output and downloading sounds and their descriptors
-  downloadedSounds = []
-  while(1):
-    if indCnt >= totalSnds:
-      print("Not able to download required number of sounds. Either there are not enough search results on freesound for your search query and filtering constraints or something is wrong with this script.")
-      break
-    sound = qRes[indCnt - ((pageNo-1)*page_size)]
-    print("Downloading mp3 preview and descriptors for sound with id: %s"%str(sound.id))
-    outDir1 = os.path.join(outputDir, queryText, str(sound.id))
-    if os.path.exists(outDir1):
-      os.system("rm -r " + outDir1)
-    os.system("mkdir " + outDir1)
-    
-    mp3Path = os.path.join(outDir1,  str(sound.previews.preview_lq_mp3.split("/")[-1]))
-    ftrPath = mp3Path.replace('.mp3', featureExt)
-    
-    try:
-      
-      fs.FSRequest.retrieve(sound.previews.preview_lq_mp3, fsClnt, mp3Path)
-      # Initialize a dictionary to store descriptors
-      features = {}
-      # Obtaining all the descriptors
-      for desc in descriptors:
-        features[desc]=[]
-        features[desc].append(eval("sound.analysis."+desc))
-      
-      # Once we have all the descriptors, store them in a json file
-      json.dump(features, open(ftrPath,'w'))
-      sndCnt+=1
-      downloadedSounds.append([str(sound.id), sound.url])
+    if API_Key == "":
+        print("\n")
+        print(
+            "You need a valid freesound API key to be able to download sounds."
+        )
+        print("Please apply for one here: www.freesound.org/apiv2/apply/")
+        print("\n")
+        return -1
 
-    except:
-      if os.path.exists(outDir1):
-        os.system("rm -r " + outDir1)
-    
-    indCnt +=1
-    
-    if indCnt%page_size==0:
-      qRes = qRes.next_page()
-      pageNo+=1
-      
-    if sndCnt>=topNResults:
-      break
-  # Dump the list of files and Freesound links
-  fid = open(os.path.join(outDir2, queryText+'_SoundList.txt'), 'w')
-  for elem in downloadedSounds:
-    fid.write('\t'.join(elem)+'\n')
-  fid.close()
-  
+    if outputDir == "" or not os.path.exists(outputDir):
+        print("\n")
+        print(
+            "Please provide a valid output directory. This will be the root directory for storing sounds and descriptors"
+        )
+        return -1
+
+    # Setting up the Freesound client and the authentication key
+    fsClnt = fs.FreesoundClient()
+    fsClnt.set_token(API_Key, "token")
+
+    # Creating a duration filter string that the Freesound API understands
+    if duration and type(duration) == tuple:
+        flt_dur = " duration:[" + str(duration[0]) + " TO " + str(
+            duration[1]) + "]"
+    else:
+        flt_dur = ""
+
+    if tag and type(tag) == str:
+        flt_tag = "tag:" + tag
+    else:
+        flt_tag = ""
+
+    # Querying Freesound
+    page_size = 30
+    if not flt_tag + flt_dur == "":
+        qRes = fsClnt.text_search(
+            query=queryText,
+            filter=flt_tag + flt_dur,
+            sort="score",
+            fields="id,name,previews,username,url,analysis",
+            descriptors=','.join(descriptors),
+            page_size=page_size,
+            normalized=1)
+    else:
+        qRes = fsClnt.text_search(
+            query=queryText,
+            sort="score",
+            fields="id,name,previews,username,url,analysis",
+            descriptors=','.join(descriptors),
+            page_size=page_size,
+            normalized=1)
+
+    outDir2 = os.path.join(outputDir, queryText)
+    if os.path.exists(
+            outDir2
+    ):  # If the directory exists, it deletes it and starts fresh
+        os.system("rm -r " + outDir2)
+    os.mkdir(outDir2)
+
+    pageNo = 1
+    sndCnt = 0
+    indCnt = 0
+    totalSnds = min(
+        qRes.count,
+        200)  # System quits after trying to download after 200 times
+
+    # Creating directories to store output and downloading sounds and their descriptors
+    downloadedSounds = []
+    while (1):
+        if indCnt >= totalSnds:
+            print(
+                "Not able to download required number of sounds. Either there are not enough search results on freesound for your search query and filtering constraints or something is wrong with this script."
+            )
+            break
+        sound = qRes[indCnt - ((pageNo - 1) * page_size)]
+        print("Downloading mp3 preview and descriptors for sound with id: %s" %
+              str(sound.id))
+        outDir1 = os.path.join(outputDir, queryText, str(sound.id))
+        if os.path.exists(outDir1):
+            os.system("rm -r " + outDir1)
+        os.system("mkdir " + outDir1)
+
+        mp3Path = os.path.join(
+            outDir1, str(sound.previews.preview_lq_mp3.split("/")[-1]))
+        ftrPath = mp3Path.replace('.mp3', featureExt)
+
+        try:
+
+            fs.FSRequest.retrieve(sound.previews.preview_lq_mp3, fsClnt,
+                                  mp3Path)
+            # Initialize a dictionary to store descriptors
+            features = {}
+            # Obtaining all the descriptors
+            for desc in descriptors:
+                features[desc] = []
+                features[desc].append(eval("sound.analysis." + desc))
+
+            # Once we have all the descriptors, store them in a json file
+            json.dump(features, open(ftrPath, 'w'))
+            sndCnt += 1
+            downloadedSounds.append([str(sound.id), sound.url])
+
+        except:
+            if os.path.exists(outDir1):
+                os.system("rm -r " + outDir1)
+
+        indCnt += 1
+
+        if indCnt % page_size == 0:
+            qRes = qRes.next_page()
+            pageNo += 1
+
+        if sndCnt >= topNResults:
+            break
+    # Dump the list of files and Freesound links
+    fid = open(os.path.join(outDir2, queryText + '_SoundList.txt'), 'w')
+    for elem in downloadedSounds:
+        fid.write('\t'.join(elem) + '\n')
+    fid.close()
